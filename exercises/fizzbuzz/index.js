cb41d8b5e31dfcc3e39a9f4b352bd861c71f97ff
Sean Padden <sean.padden7@gmail.com>
2020-01-21 11:44:18 -0500

    done

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,26 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+// create a for loop that counts up from 1 to n
+// if n is multiple of 3, print "fizz"
+// if 5, "buzz"
+// if both, "fizzbuzz"
+// else, print number
+function fizzBuzz(n) {
+  for (let i = 1; i <= n; i++){
+    if (i % 15 === 0){
+      console.log("fizzbuzz")
+    }
+    else if (i % 5 === 0){
+      console.log("buzz")
+    }
+    else if (i % 3 === 0){
+      console.log("fizz")
+    }
+    else {
+      console.log(i)
+    }
+  }
+}
 
 module.exports = fizzBuzz;
